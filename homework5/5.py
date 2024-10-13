c9388a12b7a637017160fa8c70c54df54713f330
Rulsna <mullabaevruslan@gmail.com>
2024-10-13 20:50:47 +0500

    Create 5.py

---
+++ homework5/5.py
@@ -0,0 +1,13 @@
+def get_number():
+    for num in range(1, 30, 2): 
+        yield num
+
+def main():
+    numbers = list(get_number())
+
+    print("Первое нечетное число:", numbers[0])
+    print("Пятое нечетное число:", numbers[4])
+    print("Последнее нечетное число:", numbers[-1])
+
+if __name__ == "__main__":
+    main()
